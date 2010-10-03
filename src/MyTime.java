38342d6fd352dcbaadcc2c39889e525556dc4970
Kim Burgestrand <kim@burgestrand.se>
2010-10-03 21:13:45 +0200

    Ta bort MyTime#decrement (blir fulare kod men används bara på ett ställe ändå)

--- src/MyTime.java
+++ src/MyTime.java
@@ -33,11 +33,6 @@ public class MyTime implements Cloneable {
   public void increment() {
     value++;
   }
-  
-  public void decrement(int by_value) {
-    if (by_value > value) value = 0;
-    else value -= by_value;
-  }
 
   public MyTime add(MyTime other) {
     value += other.value;
